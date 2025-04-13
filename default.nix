588a08d4b3ac5652694bf947e2a6d2292aff8bec
James <somesocks@gmail.com>
2025-04-13 11:57:04 +0200

    - updating ghc to 966 to deal with build errors on darwin-arm64

--- default.nix
+++ default.nix
@@ -9,7 +9,7 @@ let
   #     };
   #   };
   # };
-  compiler = "ghc928";
+  compiler = "ghc966";
   dhall-terraform-build = nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./build.nix {};
 in
   dhall-terraform-build
